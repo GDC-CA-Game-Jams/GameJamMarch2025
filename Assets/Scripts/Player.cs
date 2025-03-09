2b1962c0f8cbb19e0aad3352c9dc6584b4edff4e
JulieBodette <jr3python@gmail.com>
2025-03-09 15:35:25 -0400

    add player animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -21,32 +21,41 @@ public class Player : MonoBehaviour
 		if (Input.GetKey (KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
 			transform.position += moveLeft * Time.deltaTime*speed; //x,y,z
 			sr.flipX = false;
-			animator.SetBool ("isWalking", true);
+			animator.SetBool ("WalkLeftRight", true);
+			animator.SetBool ("WalkUp", false);
+			animator.SetBool ("WalkDown", false);
 		}
 
 		if (Input.GetKey (KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
 			transform.position += moveUp * Time.deltaTime*speed; //x,y,z
-			animator.SetBool ("isWalking", true);
+			animator.SetBool ("WalkUp", true);
+			animator.SetBool ("WalkLeftRight", false);
+			animator.SetBool ("WalkDown", false);
 		}
 
 		if (Input.GetKey (KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
 			transform.position += moveDown * Time.deltaTime*speed; //x,y,z
-			animator.SetBool ("isWalking", true);
+			animator.SetBool ("WalkDown", true);
+			animator.SetBool ("WalkLeftRight", false);
+			animator.SetBool ("WalkUp", false);
 		}
 
 		if (Input.GetKey (KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
 			transform.position += moveRight * Time.deltaTime*speed; //x,y,z
 			sr.flipX = true;
-			animator.SetBool ("isWalking", true);
+			animator.SetBool ("WalkLeftRight", true);
+			animator.SetBool ("WalkUp", false);
+			animator.SetBool ("WalkDown", false);
 		}
 		//if the player is not moving
 		if(!(Input.GetKey(KeyCode.W))&&!(Input.GetKey(KeyCode.A))&&!(Input.GetKey(KeyCode.S))&&!(Input.GetKey(KeyCode.D)))
 		{
 			if(!(Input.GetKey(KeyCode.UpArrow))&&!(Input.GetKey(KeyCode.LeftArrow))&&!(Input.GetKey(KeyCode.DownArrow))&&!(Input.GetKey(KeyCode.RightArrow))){
-			animator.SetBool ("isWalking", false);
+			animator.SetBool ("WalkLeftRight", false);
+			animator.SetBool ("WalkUp", false);
+			animator.SetBool ("WalkDown", false);
 			}
 		}
 			
-		speed = 4f; //in case player is done hitting barrier
     }
 }
