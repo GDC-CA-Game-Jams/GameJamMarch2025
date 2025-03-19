dc97b1e74c45fd9997bf314d469d01ddbcd0369e
JulieBodette <jr3python@gmail.com>
2025-03-19 19:48:26 -0400

    add main menu and option to play again

---
+++ Assets/Scripts/GotoScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GotoScene : MonoBehaviour
+{
+    public void OnClick(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
+
+
